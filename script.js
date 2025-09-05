b3ecfe2ce8a811abcb6b153962d509610262e00a
Jorge Garcia <garciajorgecf@gmail.com>
2025-09-04 20:25:05 -0600

    Reparando margin bottom

--- script.js
+++ script.js
@@ -14,9 +14,7 @@ window.addEventListener("load", () => {
             const progress = Math.min(scrollTop / maxScroll, 1);
             let pte = (progress * 1000) / maxScroll;
             pte = pte > 0.4 ? 0.4 : pte;
-            console.log(`${pte}`);
             title.style.transform = `scale(${1 - pte})`;
-            title.style.marginBottom = `${20-(pte*100)}vh`;
             background.style.filter = `blur(${pte*20}px) brightness(${1 - (pte*1)})`;
         });
     }
